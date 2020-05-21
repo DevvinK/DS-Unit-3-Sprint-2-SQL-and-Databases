50afc7b8822d8fa589d861417888b87bb789e4cf
DevvinK <devvink@gmail.com>
2020-05-20 19:39:15 -0600

    Connect and do simple SQL quries in python.

---
+++ module1-introduction-to-sql/app/chinook_queries.py
@@ -0,0 +1,26 @@
+# module1-introduction-to-sql\app\chinook_queries.py
+
+import os
+import sqlite3
+
+# construct a path t qo wherever your database exists
+#DB_FILEPATH = "chinook.db"
+module1-introduction-to-sql\app\chinook_queries.py
+
+connection = sqlite3.connect(DB_FILEPATH)
+connection.row_factory = sqlite3.Row # allow us to reference rows as dictionaries
+print("CONNECTION:", connection)
+
+cursor = connection.cursor()
+print("CURSOR", cursor)
+
+query = "SELECT * FROM customers LIMIT 3;"
+
+#result = cursor.execute(query)
+#print("RESULT", result) #> returns cursor object w/o results (need to fetch the results)
+
+result2 = cursor.execute(query).fetchall()
+# print("RESULT 2", result2)
+
+for row in result2:
+   print(row["CustomerId"], row["FirstName"], row["LastName"])
